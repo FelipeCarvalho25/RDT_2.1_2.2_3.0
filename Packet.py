a979edebdb3fb504137dbfc5e12161da7b35a98e
Joaocarvalho25 <69583375+Joaocarvalho25@users.noreply.github.com>
2021-10-12 17:48:45 -0300

    Commits iniciais

---
+++ Packet.py
@@ -0,0 +1,55 @@
+#Classe do pacote
+import hashlib
+
+class Packet:
+    ## the number of bytes used to store packet length
+    seq_num_S_length = 10
+    length_S_length = 10
+    ## length of md5 checksum in hex
+    checksum_length = 32
+
+    def __init__(self, seq_num, msg_S, ack_num):
+        self.seq_num = seq_num
+        self.msg_S = msg_S
+        self.ack_num = ack_num
+        self.check_sum = Packet.calculate_checkSum(msg_S)
+
+    @classmethod
+    def from_byte_S(self, byte_S):
+        if Packet.corrupt(byte_S):
+            raise RuntimeError('Cannot initialize Packet: byte_S is corrupt')
+        # extract the fields
+        seq_num = int(byte_S[Packet.length_S_length: Packet.length_S_length + Packet.seq_num_S_length])
+        msg_S = byte_S[Packet.length_S_length + Packet.seq_num_S_length + Packet.checksum_length:]
+        return self(seq_num, msg_S)
+
+    def calculate_checkSum(self,data):
+        checkSum = 1
+        return checkSum
+
+    def get_byte_S(self):
+        # convert sequence number of a byte field of seq_num_S_length bytes
+        seq_num_S = str(self.seq_num).zfill(self.seq_num_S_length)
+        # convert length to a byte field of length_S_length bytes
+        length_S = str(self.length_S_length + len(seq_num_S) + self.checksum_length + len(self.msg_S)).zfill(
+            self.length_S_length)
+        # compute the checksum
+        checksum = hashlib.md5((length_S + seq_num_S + self.msg_S).encode('utf-8'))
+        checksum_S = checksum.hexdigest()
+        # compile into a string
+        return length_S + seq_num_S + checksum_S + self.msg_S
+
+    @staticmethod
+    def corrupt(byte_S):
+        # extract the fields
+        length_S = byte_S[0:Packet.length_S_length]
+        seq_num_S = byte_S[Packet.length_S_length: Packet.seq_num_S_length + Packet.seq_num_S_length]
+        checksum_S = byte_S[
+                     Packet.seq_num_S_length + Packet.seq_num_S_length: Packet.seq_num_S_length + Packet.length_S_length + Packet.checksum_length]
+        msg_S = byte_S[Packet.seq_num_S_length + Packet.seq_num_S_length + Packet.checksum_length:]
+
+        # compute the checksum locally
+        checksum = hashlib.md5(str(length_S + seq_num_S + msg_S).encode('utf-8'))
+        computed_checksum_S = checksum.hexdigest()
+        # and check if the same
+        return checksum_S != computed_checksum_S
---
+++ aplicationClient.py
@@ -0,0 +1,1 @@
+#Esta é a camada de aplicação Sender
