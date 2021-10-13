ecb20ba75bbfc3f0e9c22dab90c5144afe75c828
Joaocarvalho25 <69583375+Joaocarvalho25@users.noreply.github.com>
2021-10-12 22:18:57 -0300

    Adicionado lógica básica do FSM do livro.

--- aplicationClient.py
+++ aplicationClient.py
@@ -1,1 +1,32 @@
 #Esta é a camada de aplicação Sender
+import  RDT_2_1
+import Packet
+
+def sender_client():
+    msg = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    state = '0'
+    port = '5002'
+    NAK = '0'
+    rdt = RDT_2_1.RDT('client', 'localhost', port)
+    packet = rdt.rdt_2_1_receive()
+    checksum = Packet.calculate_checksum(msg)
+    newPackt = Packet.makePacket(0, NAK, checksum)
+    while True:
+        if state == '0':
+            if rdt.send_ready(msg):
+                rdt.rdt_2_1_send(newPackt)
+                state = '1'
+        elif state == '1':
+            if rdt.reveid(packet) and (packet.corrupt(packet.msg_S) or packet.isNak()):
+                rdt.rdt_2_1_send(newPackt)
+            elif rdt.reveid(packet) and not packet.corrupt(packet.msg_S) and packet.isAck():
+                state = '2'
+        elif state == '2':
+            if rdt.send_ready(msg):
+                rdt.rdt_2_1_send(newPackt)
+                state = '3'
+        elif state == '3':
+            if rdt.reveid(packet) and (packet.corrupt(packet.msg_S) or packet.isNak()):
+                rdt.rdt_2_1_send(newPackt)
+            elif rdt.reveid(packet) and not packet.corrupt(packet.msg_S) and packet.isAck():
+                state = '1'
--- aplicationServer.py
+++ aplicationServer.py
@@ -1,1 +1,45 @@
 #Essa é a camada de aplicação Receive
+import  RDT_2_1
+import Packet
+
+def receiver_server(packet):
+    state = '0'
+    port = '5002'
+    NAK = '0'
+    rdt = RDT_2_1.RDT('server', None, port)
+    while True:
+        if state == '0':
+            if rdt.reveid(packet) and packet.corrupt(packet.msg_S):
+                checksum = Packet.calculate_checksum(packet.msg_S)
+                newPackt = Packet.makePacket(NAK, checksum)
+                rdt.rdt_2_1_send(newPackt)
+            elif rdt.reveid(packet) and not packet.corrupt(packet.msg_S) and packet.has_seq() == '1':
+                checksum = Packet.calculate_checksum(packet.msg_S)
+                newPackt = Packet.makePacket(NAK, checksum)
+                rdt.rdt_2_1_send(newPackt)
+            elif rdt.reveid(packet) and not packet.corrupt(packet.msg_S) and packet.has_seq() == '0':
+                data = packet.extract()
+                deliver_data(data)
+                checksum = Packet.calculate_checksum(packet.msg_S)
+                newPackt = Packet.makePacket(NAK, checksum)
+                rdt.rdt_2_1_send(newPackt)
+                state = '1'
+        elif state == '1':
+            if rdt.reveid(packet) and packet.corrupt(packet.msg_S):
+                checksum = Packet.calculate_checksum(packet.msg_S)
+                newPackt = Packet.makePacket(NAK, checksum)
+                rdt.rdt_2_1_send(newPackt)
+            elif rdt.reveid(packet) and not packet.corrupt(packet.msg_S) and packet.has_seq() == '0':
+                checksum = Packet.calculate_checksum(packet.msg_S)
+                newPackt = Packet.makePacket(NAK, checksum)
+                rdt.rdt_2_1_send(newPackt)
+            elif rdt.reveid(packet) and not packet.corrupt(packet.msg_S) and packet.has_seq() == '1':
+                data = packet.extract()
+                deliver_data(data)
+                checksum = Packet.calculate_checksum(packet.msg_S)
+                newPackt = Packet.makePacket(NAK, checksum)
+                rdt.rdt_2_1_send(newPackt)
+                state = '1'
+
+def deliver_data(data):
+    print("Receive msg" + data)
