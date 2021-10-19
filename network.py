f027365553d93927dabd5e048e9c4edcd1fb908e
Joaocarvalho25 <69583375+Joaocarvalho25@users.noreply.github.com>
2021-10-19 08:15:40 -0300

    melhorada mensagens da rede.

--- network.py
+++ network.py
@@ -11,19 +11,21 @@ def udt_send(packet, queue, range, ini=1):
 
     r = random.randrange(ini, range)
     if 10 >= r > 5:  # corrompe
-        print("Corrompendo pacote")
+        print("NETWORK - Corrompendo pacote")
         msg_corrupt = packet.extract()
         pos = random.randrange(1, 10)
         msg_corrupt[pos] = '$'
         packet.setMsg(msg_corrupt)
     elif 10 < r <= 15:  # perca de pacote
-        print("Perca de pacote ")
+        print("NETWORK - Perca de pacote ")
         packet = None
     elif 15 < r <= 20:  # atraso
-        print("Atraso pacote")
+        print("NETWORK - Atraso pacote")
         sleep(10)
+    else:
+        print("NETWORK - Pacote normal")
 
-    # fim do codigo que corrompe ou não
+        # fim do codigo que corrompe ou não
     queue.put(packet)
 
 
